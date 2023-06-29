e1d3ef0cec1b9f7dc9209bb83252afbb6eff3ab9
harish2020a <harish.2020a@vitstudent.ac.in>
2023-06-29 19:49:30 +0530

    loading state

--- src/app/components/Checkout.js
+++ src/app/components/Checkout.js
@@ -6,15 +6,18 @@ import CheckoutProduct from "./CheckoutProduct";
 import { useSession } from "next-auth/react";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
+import { useState } from "react";
 
 const stripePromise = loadStripe(process.env.stripe_public_key);
 
 const Checkout = () => {
+  const [loading, setLoading] = useState(false);
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const { data: session } = useSession();
 
   const checkoutHandler = async () => {
+    setLoading(true);
     const stripe = await stripePromise;
     const checkoutSession = await axios.post(
       "/api/create-checkout-session",
@@ -28,6 +31,7 @@ const Checkout = () => {
         },
       }
     );
+    setLoading(false);
 
     const result = await stripe.redirectToCheckout({
       sessionId: checkoutSession.data.id,
@@ -65,7 +69,7 @@ const Checkout = () => {
                     hasPrime={item.hasPrime}
                     qty={item.qty}
                   />
-                  <hr className="dashed color bg-yellow-300 h-0.5 mx-6" />
+                  <hr className="dashed color bg-yellow-300 h-0.5 my-8" />
                 </div>
               );
             })}
@@ -93,6 +97,28 @@ const Checkout = () => {
                   "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
                 }`}
               >
+                {loading && (
+                  <svg
+                    class="inline animate-spin -ml-1 mr-3 h-5 w-5 text-black"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      class="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      stroke-width="4"
+                    ></circle>
+                    <path
+                      class="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                )}
                 {!session ? "Sign in to checkout" : "Proceed to checkout"}
               </button>
             </div>
